b44b5275e24bbcc7b4c4694dd87a3497709e8ce2
Daz Devoloper Viyolen <pribadiarep2@gmail.com>
2025-05-30 13:39:24 +0700

    Create waPairing.js

---
+++ plugins/waPairing.js
@@ -0,0 +1,33 @@
+const {
+  makeWASocket,
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion,
+  makeCacheableSignalKeyStore
+} = require('@fizzxydev/baileys-pro');
+
+const pino = require('pino');
+const NodeCache = require('node-cache');
+
+async function connectWithPairing(phoneNumber) {
+  const { state, saveCreds } = await useMultiFileAuthState('Storage/session');
+  const { version } = await fetchLatestBaileysVersion();
+  const sock = makeWASocket({
+    version,
+    auth: {
+      creds: state.creds,
+      keys: makeCacheableSignalKeyStore(state.keys, pino().child({ level: 'silent' }))
+    },
+    logger: pino({ level: 'silent' }),
+    browser: ['Ubuntu', 'Chrome', '20.0.04'],
+    printQRInTerminal: false,
+  });
+
+  if (!sock.authState.creds.registered) {
+    const code = await sock.requestPairingCode(phoneNumber, 'NETLIFYBOT');
+    return { status: 'success', pairingCode: code };
+  }
+
+  return { status: 'already_connected' };
+}
+
+module.exports = { connectWithPairing };
