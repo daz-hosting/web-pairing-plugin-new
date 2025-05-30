93b1367005c3cab7fdc487cdecf8836a34289872
Daz Devoloper Viyolen <pribadiarep2@gmail.com>
2025-05-30 15:45:20 +0700

    Update download.js

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,16 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { file } = req.query;
-  if (!file) return res.status(400).send('File tidak ditemukan');
+  if (!file) return res.status(400).json({ message: 'File tidak ditemukan' });
 
   const filePath = path.join(process.cwd(), 'downloads', file);
-  if (fs.existsSync(filePath)) {
-    res.setHeader('Content-Disposition', `attachment; filename="${file}"`);
-    res.setHeader('Content-Type', 'application/zip');
-    fs.createReadStream(filePath).pipe(res);
-  } else {
-    res.status(404).send('File tidak ditemukan');
-  }
+  if (!fs.existsSync(filePath)) return res.status(404).json({ message: 'File tidak ditemukan' });
+
+  res.setHeader('Content-Type', 'application/zip');
+  res.setHeader('Content-Disposition', `attachment; filename="${file}"`);
+  fs.createReadStream(filePath).pipe(res);
 }
